96c4ff3a11fdd937f4c8cf9a0f3b20bf86405779
Mathieu Bolard <mathieu.bolard@gmail.com>
2013-05-04 12:10:45 +0200

    new SixAxisStick graphics
    new SixAxisStick graphics

--- PS3SixAxisTest/SixAxisStick.m
+++ PS3SixAxisTest/SixAxisStick.m
@@ -14,17 +14,19 @@
 - (id) initWithFrame:(NSRect)frameRect {
 	if(self = [super initWithFrame:frameRect]) {
 		[self setWantsLayer:YES];
+        [[self layer] setMasksToBounds:NO];
 		
 		CGColorRef backColor = CGColorCreateGenericRGB(1.00f, 1.00f, 1.00f, 0.00f);
 		
 		point = [CALayer layer];
-        [point setBounds:CGRectMake(0, 0, 40, 40)];
-		[point setAnchorPoint:CGPointMake(0, 0)];
+        [point setBounds:CGRectMake(0, 0, frameRect.size.width*0.65, frameRect.size.height*0.65)];
+        [point setMasksToBounds:NO];
+		[point setAnchorPoint:CGPointMake(0.5, 0.5)];
 		[point setShadowOpacity:.8f];
-		[point setCornerRadius:20];
+		[point setCornerRadius:frameRect.size.width*0.325];
 		[[self layer] setBackgroundColor:backColor];
 		[[self layer] addSublayer:point];
-		
+        		
 		[self setJoyStickX:128 Y:128 pressed:NO];
 	}
 	return self;
@@ -32,15 +34,35 @@
 
 - (void) setJoyStickX:(NSInteger)x Y:(NSInteger)y pressed:(BOOL)isPressed {
 	NSSize size = [self frame].size;
-	float scale = size.width / (255 + 20);
+	float scale = (size.width-2.0) / 255.0;
 	CGColorRef pointColor;
 	if (isPressed) {
 		pointColor = CGColorCreateGenericRGB(1.00f, 0.00f, 0.00f, 1.00f);
 	} else {
-		pointColor = CGColorCreateGenericRGB(0.25f, 0.25f, 0.25f, 1.00f);
+		pointColor = CGColorCreateGenericRGB(0.236, 0.236, 0.236, 1.00f);
 	}
 	[point setBackgroundColor:pointColor];
-	[point setPosition:CGPointMake(x * scale - 20, size.height - (y * scale) - 20)];
+	[point setPosition:CGPointMake(x * scale + 1.0, size.height - (y * scale) + 1.0)];
+}
+
+- (void) drawRect:(NSRect)dirtyRect {
+
+    NSColor* blackColor = [NSColor colorWithCalibratedRed: 0 green: 0 blue: 0 alpha: 1];
+    NSColor* greyColor = [NSColor colorWithCalibratedRed: 0.151 green: 0.151 blue: 0.151 alpha: 1];
+    
+    {
+        NSBezierPath* border = [NSBezierPath bezierPathWithOvalInRect:CGRectMake(1.0, 1.0, dirtyRect.size.width-2.0, dirtyRect.size.height-2.0)];
+        [greyColor setFill];
+        [border fill];
+        [blackColor setStroke];
+        [border setLineWidth: 1];
+        [border stroke];
+        
+        NSBezierPath* blackpart = [NSBezierPath bezierPathWithOvalInRect:CGRectMake(17.0, 17.0, dirtyRect.size.width-34.0, dirtyRect.size.height-34.0)];
+        [blackColor setFill];
+        [blackpart fill];
+    }
+
 }
 
 @end
